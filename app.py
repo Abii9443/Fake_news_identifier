d01e7c4502c9e347c0e9f8b0871dafab7d2f803c
Abilash Hari <46106987+Abii9443@users.noreply.github.com>
2022-07-18 19:17:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # from urllib import request
 
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,make_response
 from sklearn.pipeline import Pipeline
 import joblib
 from sklearn import linear_model
@@ -11,6 +11,22 @@ import feature as fp
 import PyPDF2
 app = Flask(__name__)
 loaded_model = joblib.load('news.sav')
+def download_data(num):
+
+    if num == 2:
+        response = make_response(open('news_2.pdf', 'rb').read())
+
+        return response
+    # if num == 3:
+    #     response = make_response(open('news_3.pdf', 'rb').read())
+    #     response.headers['Content-Type'] = 'pdf'
+    #     response.headers["Content-Disposition"] = "attachment; filename=data-3.pdf"
+    #     return response
+    # if num == '4':
+    #     response = make_response(open('news_4.pdf', 'rb').read())
+    #     response.headers['Content-Type'] = 'pdf'
+    #     response.headers["Content-Disposition"] = "attachment; filename=data-4.pdf"
+    #     return response
 
 @app.route('/')
 def index():  # put application's code here
@@ -24,10 +40,40 @@ def upload():
         pageObj = pdfReader.getPage(0)
 
         sentence = (pageObj.extractText())
-        # print(sentence)
+            # print(sentence)
         result= fp.manual_testing(sentence)
+    # if request.form['news1'] == '1':
+    #     download(1)
+    return render_template("sucess.html", result=result)
+@app.route("/download",methods=["POST"])
+def download():
+    if request.method == 'POST':
+        data1=request.form.get('news1')
+        data2=request.form.get('news2')
+        data3=request.form.get('news3')
+        data4=request.form.get('news4')
+        if(data1=='1'):
+            response = make_response(open('news_1.pdf', 'rb').read())
+            response.headers['Content-Type'] = 'pdf'
+            response.headers["Content-Disposition"] = "attachment; filename=data-1.pdf"
+            return response
+        if(data2=='2'):
+            response = make_response(open('news_2.pdf', 'rb').read())
+            response.headers['Content-Type'] = 'pdf'
+            response.headers["Content-Disposition"] = "attachment; filename=data-2.pdf"
+            return response
+        if(data3=='3'):
+            response = make_response(open('news_3.pdf', 'rb').read())
+            response.headers['Content-Type'] = 'pdf'
+            response.headers["Content-Disposition"] = "attachment; filename=data-3.pdf"
+            return response
+        if(data4=='4'):
+            response = make_response(open('news_4.pdf', 'rb').read())
+            response.headers['Content-Type'] = 'pdf'
+            response.headers["Content-Disposition"] = "attachment; filename=data-4.pdf"
+            return response
 
-    return render_template("sucess.html",result=result)
+
 
 if __name__ == '__main__':
     app.run()
